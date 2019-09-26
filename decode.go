a1c1917314689ef2efa99435999d97d3a9d12364
sodefrin <kojitazoe@gmail.com>
2019-09-26 23:36:05 +0900

    Initial commit

---
+++ decode.go
@@ -0,0 +1,123 @@
+package csvdecoder
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+var (
+	ErrInvalidInterface = errors.New("invalid interface")
+	ErrParse            = errors.New("parse error")
+)
+
+func Decode(out interface{}, r io.Reader) error {
+	v := reflect.ValueOf(out)
+	if v.Kind() != reflect.Ptr {
+		return fmt.Errorf("%w; out must be pointer", ErrInvalidInterface)
+	}
+	v = v.Elem()
+	if v.Kind() != reflect.Slice {
+		return fmt.Errorf("%w; out must be slice", ErrInvalidInterface)
+	}
+
+	csvreader := csv.NewReader(r)
+
+	column, err := csvreader.Read()
+	if err != nil {
+		return err
+	}
+
+	for {
+		values, err := csvreader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		elem := reflect.New(v.Type().Elem()).Elem()
+
+		if err := decode(elem, column, values); err != nil {
+			return err
+		}
+		v.Set(reflect.Append(v, elem))
+	}
+
+	return nil
+}
+
+func decode(v reflect.Value, column, values []string) error {
+	if v.Kind() == reflect.Ptr {
+		v.Set(reflect.New(v.Type().Elem()))
+		return decode(v.Elem(), column, values)
+	}
+
+	if v.Kind() != reflect.Struct {
+		return fmt.Errorf("%w; v must be slice but have %s", ErrInvalidInterface, v.Kind())
+	}
+
+	valueMap := map[string]string{}
+	if len(column) != len(values) {
+		return fmt.Errorf("%w; column must have %d elems", ErrParse, len(column))
+	}
+
+	for i, key := range column {
+		valueMap[key] = values[i]
+	}
+
+	return decodeStruct(v, valueMap)
+}
+
+func decodeStruct(v reflect.Value, values map[string]string) error {
+	for i := 0; i < v.NumField(); i++ {
+		name := v.Type().Field(i).Name
+		value := v.FieldByName(name)
+		str, ok := values[name]
+		if !ok || str == "" {
+			break
+		}
+		switch value.Interface().(type) {
+		case int, int8, int16, int32, int64:
+			ret, err := strconv.ParseInt(str, 10, 64)
+			if err != nil {
+				return err
+			}
+			value.SetInt(ret)
+		case uint, uint8, uint16, uint32, uint64:
+			ret, err := strconv.ParseUint(str, 10, 64)
+			if err != nil {
+				return err
+			}
+			value.SetUint(ret)
+		case float32, float64:
+			ret, err := strconv.ParseFloat(str, 64)
+			if err != nil {
+				return err
+			}
+			value.SetFloat(ret)
+		case bool:
+			ret, err := strconv.ParseBool(str)
+			if err != nil {
+				return err
+			}
+			value.SetBool(ret)
+		case string:
+			value.SetString(str)
+		case time.Time:
+			ret, err := time.Parse(time.RFC3339, str)
+			if err != nil {
+				return err
+			}
+			value.Set(reflect.ValueOf(ret))
+		default:
+			return fmt.Errorf("%w; unknown field type", ErrParse)
+		}
+	}
+
+	return nil
+}
---
+++ decode_test.go
@@ -0,0 +1,57 @@
+package csvdecoder
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+)
+
+func TestDecode(t *testing.T) {
+	type A struct {
+		Test string
+	}
+
+	t.Run("invalid not slice", func(t *testing.T) {
+		data := []byte("")
+		out := A{}
+		r := bytes.NewReader(data)
+		if err := Decode(out, r); !errors.Is(err, ErrInvalidInterface) {
+			t.Fatalf("want err: %s, but have: %s", ErrInvalidInterface, err)
+		}
+	})
+
+	t.Run("invliad not pointer", func(t *testing.T) {
+		data := []byte("")
+		out := []A{}
+		r := bytes.NewReader(data)
+		if err := Decode(out, r); !errors.Is(err, ErrInvalidInterface) {
+			t.Fatalf("want err: %s, but have: %s", ErrInvalidInterface, err)
+		}
+	})
+
+	t.Run("success slice struct", func(t *testing.T) {
+		data := []byte("Test\na\n")
+		out := []A{}
+		r := bytes.NewReader(data)
+		if err := Decode(&out, r); err != nil {
+			t.Fatal(err)
+		}
+
+		if out[0].Test != "a" {
+			t.Fatalf("out.Test must be a")
+		}
+	})
+
+	t.Run("success slice pointer", func(t *testing.T) {
+		data := []byte("Test\na\n")
+		out := []*A{}
+		r := bytes.NewReader(data)
+		if err := Decode(&out, r); err != nil {
+			t.Fatal(err)
+		}
+
+		if out[0].Test != "a" {
+			t.Fatalf("out.Test must be a")
+		}
+	})
+}
